c35a58660b94c82952f93c86179d4cea306f6515
xinping <xpws2006@163.com>
2018-01-09 00:01:43 +0800

    redis集群

---
+++ Chapter07/TestRedisPro/src/com/redis/TestCluster.java
@@ -0,0 +1,28 @@
+package com.redis;
+
+import java.util.HashSet;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+
+
+public class TestCluster {
+	 
+	 public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			HashSet<HostAndPort> set = new HashSet<HostAndPort>();
+			set.add(new HostAndPort("192.168.1.13", 7000));
+			set.add(new HostAndPort("192.168.1.13", 7001));
+			set.add(new HostAndPort("192.168.1.13", 7002));
+			
+			set.add(new HostAndPort("192.168.1.14", 7003));
+			set.add(new HostAndPort("192.168.1.14", 7004));
+			set.add(new HostAndPort("192.168.1.14", 7005));
+			
+			JedisCluster cluster = new JedisCluster(set);
+			//redis内部会创建连接池，从连接池中获取连接使用，然后再把连接返回给连接池
+			cluster.set("greeting", "hello redis");
+			
+		}
+
+}
