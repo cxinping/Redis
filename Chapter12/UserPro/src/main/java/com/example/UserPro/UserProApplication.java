45aa6531f94df6a0fa3d9cb6a5e5100eee2ce73f
xinping <xpws2006@163.com>
2018-03-31 20:01:55 +0800

    restful

---
+++ Chapter12/UserPro/src/main/java/com/example/UserPro/UserProApplication.java
@@ -0,0 +1,12 @@
+package com.example.UserPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProApplication.class, args);
+	}
+}
