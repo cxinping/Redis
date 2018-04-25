2eb4a0c00696d3479edbb9028d23c763f080c1ab
xinping <xpws2006@163.com>
2018-04-25 20:56:03 +0800

    优化 redis web项目

--- Chapter12/MyWeb/src/main/java/com/redis/web/base/RestClient.java
+++ Chapter12/MyWeb/src/main/java/com/redis/web/base/RestClient.java
@@ -1,6 +1,5 @@
 package com.redis.web.base;
 
-
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Enumeration;
@@ -28,7 +27,7 @@ import com.redis.web.controller.RestHandle;
 public class RestClient {
 	private Logger logger = LoggerFactory.getLogger(RestHandle.class);
 
-	public HttpResponse getResponse(HttpUriRequest request){
+	public HttpResponse getResponse(HttpUriRequest request) {
 		DefaultHttpClient client = new DefaultHttpClient();
 		HttpResponse httpResponse = null;
 		try {
@@ -44,25 +43,27 @@ public class RestClient {
 		return httpResponse;
 	}
 
-	public String getResponseAsString(HttpUriRequest request){
-		
+	public String getResponseAsString(HttpUriRequest request) {
+
 		DefaultHttpClient client = new DefaultHttpClient();
 		try {
 			HttpResponse httpResponse = client.execute(request);
 			HttpEntity entity = httpResponse.getEntity();
 			if (entity != null) {
+				//设置字符集为utf-8
 				return EntityUtils.toString(entity, "UTF-8");
 			}
 		} catch (Exception e) {
+			//数据请求失败
 			e.printStackTrace();
 		} finally {
 			client.getConnectionManager().shutdown();
 		}
 		return null;
 	}
-	
-	public void getResponseAsStream(HttpUriRequest request,HttpServletResponse response){
-		
+
+	public void getResponseAsStream(HttpUriRequest request, HttpServletResponse response) {
+
 		DefaultHttpClient client = new DefaultHttpClient();
 		try {
 			HttpResponse httpResponse = client.execute(request);
@@ -70,30 +71,30 @@ public class RestClient {
 			Header[] headers = httpResponse.getAllHeaders();
 			int statCode = httpResponse.getStatusLine().getStatusCode();
 			response.setStatus(statCode);
-			for(int i=0;i<headers.length;i++) {
+			for (int i = 0; i < headers.length; i++) {
 				String key = headers[i].getName();
-		        String value = headers[i].getValue();
-				if(!"content-length".equals(key.toLowerCase())){
-					if("content-type".equals(key.toLowerCase()) && value.indexOf("charset") == -1){
-						value = value +";charset=utf-8";
+				String value = headers[i].getValue();
+				if (!"content-length".equals(key.toLowerCase())) {
+					if ("content-type".equals(key.toLowerCase()) && value.indexOf("charset") == -1) {
+						value = value + ";charset=utf-8";
 						logger.info("reset Content-Type to add charset=utf-8");
 					}
-					
-					logger.info("getResponseAsStream set header : {} = {}",key,value);
+
+					logger.info("getResponseAsStream set header : {} = {}", key, value);
 					response.setHeader(key, value);
 				}
 			}
 			OutputStream os = response.getOutputStream();
-			if (entity != null && entity.isStreaming()){
-				InputStream is= entity.getContent();
-				byte b[] = new byte[1024];  
-                int j = 0;  
-                while ((j = is.read(b)) != -1) {  
-                    os.write(b, 0, j);  
-                }  
-                os.flush();  
-                os.close(); 
-                is.close();
+			if (entity != null && entity.isStreaming()) {
+				InputStream is = entity.getContent();
+				byte b[] = new byte[1024];
+				int j = 0;
+				while ((j = is.read(b)) != -1) {
+					os.write(b, 0, j);
+				}
+				os.flush();
+				os.close();
+				is.close();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -101,123 +102,122 @@ public class RestClient {
 			client.getConnectionManager().shutdown();
 		}
 	}
-	
-	
-	public String get(String url,String authToken){
+
+	public String get(String url, String authToken) {
+		//设置请求方法get
 		HttpGet get = new HttpGet(url);
 		if (authToken != null) {
 			get.setHeader("authToken", authToken);
 		}
 		return getResponseAsString(get);
 	}
-	
-	public void get(String url ,HttpServletRequest request,HttpServletResponse response){
+
+	public void get(String url, HttpServletRequest request, HttpServletResponse response) {
 		HttpGet get = new HttpGet(url);
 		Enumeration headerNames = request.getHeaderNames();
-	    while (headerNames.hasMoreElements()) {
-	        String key = (String) headerNames.nextElement();
-	        String value = request.getHeader(key);
-	        if (!"content-length".equals(key.toLowerCase())){
-	        	logger.info("set header : {} = {}",key,value);
-	        	get.setHeader(key, value);
-	        }	
-	    }
-		getResponseAsStream(get,response);
+		while (headerNames.hasMoreElements()) {
+			String key = (String) headerNames.nextElement();
+			String value = request.getHeader(key);
+			if (!"content-length".equals(key.toLowerCase())) {
+				logger.info("set header : {} = {}", key, value);
+				get.setHeader(key, value);
+			}
+		}
+		getResponseAsStream(get, response);
 	}
-	
 
-	public String delete(String url ){
+	public String delete(String url) {
+		// 设置请求方法delete
 		HttpDelete delete = new HttpDelete(url);
 		return getResponseAsString(delete);
 	}
-	
-	public void delete(String url ,HttpServletRequest request,HttpServletResponse response){
+
+	public void delete(String url, HttpServletRequest request, HttpServletResponse response) {
+		// 设置请求方法delete
 		HttpDelete delete = new HttpDelete(url);
 		Enumeration headerNames = request.getHeaderNames();
-	    while (headerNames.hasMoreElements()) {
-	        String key = (String) headerNames.nextElement();
-	        String value = request.getHeader(key);
-	        if (!"content-length".equals(key.toLowerCase())){
-	        	logger.info("set header : {} = {}",key,value);
-	        	delete.setHeader(key, value);
-	        }	
-	    }
-		getResponseAsStream(delete,response);
+		while (headerNames.hasMoreElements()) {
+			String key = (String) headerNames.nextElement();
+			String value = request.getHeader(key);
+			if (!"content-length".equals(key.toLowerCase())) {
+				logger.info("set header : {} = {}", key, value);
+				delete.setHeader(key, value);
+			}
+		}
+		getResponseAsStream(delete, response);
 	}
-	
-	public String post(String url,String json,HttpServletRequest request){
+
+	public String post(String url, String json, HttpServletRequest request) {
+		// 设置请求方法post
 		HttpPost post = new HttpPost(url);
-		if (json != null ) {
-			post.setEntity(new StringEntity(json,ContentType.APPLICATION_JSON));
+		if (json != null) {
+			post.setEntity(new StringEntity(json, ContentType.APPLICATION_JSON));
 		}
 		Enumeration headerNames = request.getHeaderNames();
-	    while (headerNames.hasMoreElements()) {
-	        String key = (String) headerNames.nextElement();
-	        String value = request.getHeader(key);
-	        if (!"content-length".equals(key.toLowerCase())){
-	        	logger.info("set header : {} = {}",key,value);
-	        	post.setHeader(key, value);
-	        }	
-	    }
+		while (headerNames.hasMoreElements()) {
+			String key = (String) headerNames.nextElement();
+			String value = request.getHeader(key);
+			if (!"content-length".equals(key.toLowerCase())) {
+				logger.info("set header : {} = {}", key, value);
+				post.setHeader(key, value);
+			}
+		}
 		return getResponseAsString(post);
-		
 	}
-	
-	public void post(String url,String json
-			,HttpServletRequest request,HttpServletResponse response){
+
+	public void post(String url, String json, HttpServletRequest request, HttpServletResponse response) {
+		// 设置请求方法post
 		HttpPost post = new HttpPost(url);
-		if (json != null ) {
-			post.setEntity(new StringEntity(json,ContentType.APPLICATION_JSON));
+		if (json != null) {
+			post.setEntity(new StringEntity(json, ContentType.APPLICATION_JSON));
 		}
 		Enumeration headerNames = request.getHeaderNames();
-	    while (headerNames.hasMoreElements()) {
-	        String key = (String) headerNames.nextElement();
-	        String value = request.getHeader(key);
-	        if (!"content-length".equals(key.toLowerCase())){
-	        	logger.info("set header : {} = {}",key,value);
-	        	post.setHeader(key, value);
-	        }	
-	    }
-		getResponseAsStream(post,response);
+		while (headerNames.hasMoreElements()) {
+			String key = (String) headerNames.nextElement();
+			String value = request.getHeader(key);
+			if (!"content-length".equals(key.toLowerCase())) {
+				logger.info("set header : {} = {}", key, value);
+				post.setHeader(key, value);
+			}
+		}
+		getResponseAsStream(post, response);
 	}
-	
-	public void put(String url,String json
-			,HttpServletRequest request,HttpServletResponse response){
+
+	public void put(String url, String json, HttpServletRequest request, HttpServletResponse response) {
 		HttpPut put = new HttpPut(url);
-		
-		if (json != null ) {
-			put.setEntity(new StringEntity(json,ContentType.APPLICATION_JSON));
+
+		if (json != null) {
+			put.setEntity(new StringEntity(json, ContentType.APPLICATION_JSON));
 		}
 		Enumeration headerNames = request.getHeaderNames();
-	    while (headerNames.hasMoreElements()) {
-	        String key = (String) headerNames.nextElement();
-	        String value = request.getHeader(key);
-	        if (!"content-length".equals(key.toLowerCase())){
-	        	logger.info("set header : {} = {}",key,value);
-	        	put.setHeader(key, value);
-	        }
-	    }
-	    getResponseAsStream(put,response);		
-		
+		while (headerNames.hasMoreElements()) {
+			String key = (String) headerNames.nextElement();
+			String value = request.getHeader(key);
+			if (!"content-length".equals(key.toLowerCase())) {
+				logger.info("set header : {} = {}", key, value);
+				put.setHeader(key, value);
+			}
+		}
+		getResponseAsStream(put, response);
 	}
-	
-	public String put(String url,String json,HttpServletRequest request){
+
+	public String put(String url, String json, HttpServletRequest request) {
+		//设置请求方法put
 		HttpPut put = new HttpPut(url);
-		
-		if (json != null ) {
-			put.setEntity(new StringEntity(json,ContentType.APPLICATION_JSON));
+
+		if (json != null) {
+			put.setEntity(new StringEntity(json, ContentType.APPLICATION_JSON));
 		}
 		Enumeration headerNames = request.getHeaderNames();
-	    while (headerNames.hasMoreElements()) {
-	        String key = (String) headerNames.nextElement();
-	        String value = request.getHeader(key);
-	        if (!"content-length".equals(key.toLowerCase())){
-	        	logger.info("set header : {} = {}",key,value);
-	        	put.setHeader(key, value);
-	        }
-	    }
-		return getResponseAsString(put);		
-		
+		while (headerNames.hasMoreElements()) {
+			String key = (String) headerNames.nextElement();
+			String value = request.getHeader(key);
+			if (!"content-length".equals(key.toLowerCase())) {
+				logger.info("set header : {} = {}", key, value);
+				put.setHeader(key, value);
+			}
+		}
+		return getResponseAsString(put);
 	}
 
 }
