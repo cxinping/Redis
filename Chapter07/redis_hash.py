1da93946246405082759b638270a08934657ddbe
xpws2006@163.com <123@welcome>
2018-05-09 14:44:40 +0800

    使用Python Redis模块 操作hash类型

--- Chapter07/redis_hash.py
+++ Chapter07/redis_hash.py
@@ -13,5 +13,13 @@ r.hset('user','name','xinping')
 print('\n#2')
 r.hmset('user',{ 'name': 'lisi' ,'age':20 })
 
+#3
+print('\n#3')
+r.hset('user','name','xinping')
+print( r.hget('user', 'name') )
 
-
+#4
+print('\n#4')
+r.hmset('user',{ 'name': 'lisi' ,'age':20 })
+print( r.hmget('user', ['name', 'age']) )
+print( r.hmget('user', 'name', 'age') )
