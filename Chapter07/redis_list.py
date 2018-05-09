6e7b643a01e616cdbb1b4d43486975f2a5763cc4
xpws2006@163.com <123@welcome>
2018-05-09 17:48:03 +0800

    使用Python Redis模块 操作list类型

--- Chapter07/redis_list.py
+++ Chapter07/redis_list.py
@@ -9,11 +9,18 @@ r.lpush('nums' , 1,2,3)
 
 #2
 print('\n#2')
+r.delete('nums')
+r.lpush('nums' , 1,2,3)
 r.linsert('nums' , where= 'before', refvalue='2' , value='a')
 
 #3
 print('\n#3')
+r.delete('nums')
+r.lpush('nums' , 1,2,3)
+r.lset('nums', 2, 'b')
 
-
-
-
+#4
+print('\n#4')
+r.delete('nums')
+r.lpush('nums' , 1,2,3)
+#r.lrem('nums', 3, 1)
