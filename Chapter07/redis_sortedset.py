b9181cfcef7135132dd086d24379eb6332a78393
xpws2006@163.com <123@welcome>
2018-05-09 20:18:48 +0800

    使用Python Redis模块 操作set类型

--- Chapter07/redis_sortedset.py
+++ Chapter07/redis_sortedset.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 
 import redis
-
 pool = redis.ConnectionPool(host='127.0.0.1', port=6379)
 r = redis.Redis(connection_pool=pool)
 
 #1
 print('\n#1')
+r.sadd('sets', 1,2,3,4)
+
+#2
+print('\n#2')
+r.delete('sets')
+r.sadd('sets', 1,2,3,4)
+print( r.scard('sets') )
+
+#3
+print('\n#3')
+
+
+
+
+
 
 
