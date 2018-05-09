dcb773ef637055444f46d597a3008571953458f5
xpws2006@163.com <123@welcome>
2018-05-09 17:00:56 +0800

    使用Python Redis模块 操作list类型

--- Chapter07/redis_list.py
+++ Chapter07/redis_list.py
@@ -5,7 +5,14 @@ r = redis.Redis(connection_pool=pool)
 
 #1
 print('\n#1')
+r.lpush('nums' , 1,2,3)
 
+#2
+print('\n#2')
+r.linsert('nums' , where= 'before', refvalue='2' , value='a')
+
+#3
+print('\n#3')
 
 
 
