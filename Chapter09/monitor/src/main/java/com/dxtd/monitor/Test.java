782ffa189b9ce498b1484646afbe7ba7bfea4a1c
cxinping <templarz334422>
2019-09-11 15:34:01 +0800

    优化代码

--- Chapter09/monitor/src/main/java/com/dxtd/monitor/Test.java
+++ Chapter09/monitor/src/main/java/com/dxtd/monitor/Test.java
@@ -1,32 +1,58 @@
 package com.dxtd.monitor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import redis.clients.jedis.Jedis;
 
 public class Test {
 
 	public String getInfo() {
-        String infoContent = null;
-        Jedis jedis = null;
-        try {
-        	jedis = RedisUtils.getJedis();
-            infoContent = jedis.info();
-        }catch(Exception e) {
-        	e.printStackTrace();
-        } 
-        finally {
-            if (jedis != null) {
-            	jedis.close();
-            }
-        }
-        return infoContent;
-    }
+		String infoContent = null;
+		Jedis jedis = null;
+		try {
+			jedis = RedisUtils.getJedis();
+			infoContent = jedis.info();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (jedis != null) {
+				jedis.close();
+			}
+		}
+		return infoContent;
+	}
+
+	public Map parseInfo(String content) {
+		String[] lines = content.split("\n");
+		Map infoMap = new HashMap();
+		String part = null;
+		for (String line : lines) {
+			if (line.isEmpty()) {
+				continue;
+			}
+
+			if (line.startsWith("#")) {
+				part = line.replace("#", "").trim();
+				continue;
+			}
+
+			int index = line.indexOf(':');
+			if (index >= 0) {
+				infoMap.put(part + "." + line.substring(0, index), line.substring(index + 1));
+			}
+		}
+
+		return infoMap;
+	}
 
 	public static void main(String[] args) {
 		Test test = new Test();
 		String info = test.getInfo();
-		System.out.println(info);
-		
+		// System.out.println(info);
 
+		Map map = test.parseInfo(info);
+		System.out.println("map=" +map);
 	}
 
 }
