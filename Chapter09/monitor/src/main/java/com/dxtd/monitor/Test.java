00f09eea72ed6939196373c3ed5fafb0b5ec9f92
cxinping <templarz334422>
2019-09-12 09:09:33 +0800

    优化代码

--- Chapter09/monitor/src/main/java/com/dxtd/monitor/Test.java
+++ Chapter09/monitor/src/main/java/com/dxtd/monitor/Test.java
@@ -23,6 +23,14 @@ public class Test {
 		return infoContent;
 	}
 
+	Double getIntValue(Map<String, String> infoMap, String key) {
+		if (null == infoMap)
+			return 0.0;
+
+		Double value = Double.valueOf(infoMap.get(key));
+		return value;
+	}
+
 	public Map parseInfo(String content) {
 		String[] lines = content.split("\n");
 		Map infoMap = new HashMap();
@@ -46,13 +54,37 @@ public class Test {
 		return infoMap;
 	}
 
+	public void transData(String infoContent) {
+		Map<String, String> infoMap = parseInfo(infoContent);
+		Statement stat = new Statement();
+		stat.setUsed_cpu_sys(getIntValue(infoMap, "CPU.used_cpu_sys"));
+		stat.setUsed_cpu_user(getIntValue(infoMap, "CPU.used_cpu_user"));
+
+		stat.setBlocked_clients(getIntValue(infoMap, "Clients.blocked_clients"));
+		stat.setConnected_clients(getIntValue(infoMap, "Clients.connected_clients"));
+		stat.setUsed_memory(getIntValue(infoMap, "Memory.used_memory"));
+		stat.setUsed_memory_rss(getIntValue(infoMap, "Memory.used_memory_rss"));
+
+		//
+		double cmd = getIntValue(infoMap, "Stats.total_commands_processed");
+		double exp = getIntValue(infoMap, "Stats.expired_keys");
+		double evt = getIntValue(infoMap, "Stats.evicted_keys");
+
+		double hit = getIntValue(infoMap, "Stats.keyspace_hits");
+		double mis = getIntValue(infoMap, "Stats.keyspace_misses");
+
+		// long lastTs = stat.getTimestamp();
+		long thisTs = System.currentTimeMillis();
+		System.out.println(stat);
+	}
+
 	public static void main(String[] args) {
 		Test test = new Test();
 		String info = test.getInfo();
-		// System.out.println(info);
+		// Map map = test.parseInfo(info);
+		// System.out.println("map=" +map);
+		test.transData(info);
 
-		Map map = test.parseInfo(info);
-		System.out.println("map=" +map);
 	}
 
 }
--- Chapter10/UserPro/src/main/java/com/example/user/service/IUserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.user.service;
-
-import com.example.user.domain.User;
-
-public interface IUserService {
-    void addUser(User user);
-    
-}
