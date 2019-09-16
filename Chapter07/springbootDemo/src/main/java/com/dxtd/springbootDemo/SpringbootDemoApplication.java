3507ec4ee034d34de1e295d92cd7cf4f31afd26a
cxinping <templarz334422>
2019-09-16 13:46:33 +0800

    编写Spring Boot的Hello World

--- Chapter07/springbootDemo/src/main/java/com/dxtd/springbootDemo/SpringbootDemoApplication.java
+++ Chapter07/springbootDemo/src/main/java/com/dxtd/springbootDemo/SpringbootDemoApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootDemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
 
---
+++ Chapter07/springbootDemo/src/main/java/com/dxtd/springbootDemo/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.dxtd.springbootDemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    // http://localhost:8080/hello
+    @RequestMapping("/hello")
+    public String index() {
+
+        return "Hello World spring boot bbb 333";
+    }
+
+
+}
