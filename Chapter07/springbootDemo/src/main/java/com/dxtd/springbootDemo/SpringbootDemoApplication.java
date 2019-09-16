e22c4db66f1a2ea82b2edf8d2e52a4597a2e46e0
cxinping <templarz334422>
2019-09-16 10:53:19 +0800

    编写Spring Boot的Hello World

---
+++ Chapter07/springbootDemo/src/main/java/com/dxtd/springbootDemo/SpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dxtd.springbootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+	}
+
+}
---
+++ Chapter07/springbootDemo/src/test/java/com/dxtd/springbootDemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxtd.springbootDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
