c962ededbc280f8b44b2c038f89c1d8d15208728
cxinping <templarz334422>
2019-09-23 18:16:34 +0800

    Redis集群

---
+++ Chapter05/redis_cluster_example/src/main/java/com/dxtd/test/RedisCLusterDemo.java
@@ -0,0 +1,32 @@
+package com.dxtd.test;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+
+import static sun.management.jmxremote.ConnectorBootstrap.PropertyNames.HOST;
+
+public class RedisCLusterDemo {
+
+    public static void testJedisCluster()  {
+        HashSet<HostAndPort> nodes = new HashSet<HostAndPort>();
+        nodes.add(new HostAndPort("192.168.11.10", 7000));
+        nodes.add(new HostAndPort("192.168.11.10", 7001));
+        nodes.add(new HostAndPort("192.168.11.10", 7002));
+        nodes.add(new HostAndPort("192.168.11.11", 7003));
+        nodes.add(new HostAndPort("192.168.11.11", 7004));
+        nodes.add(new HostAndPort("192.168.11.11", 7005));
+        JedisCluster cluster = new JedisCluster(nodes);
+        cluster.set("cluster-key", "cluster-value");
+        System.out.println("集群测试 ： " + cluster.get("cluster-key"));
+        cluster.close();
+    }
+
+    public static void main(String[] args){
+        testJedisCluster();
+    }
+
+}
