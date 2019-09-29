1b1e8c1a10d2ce653c18fff676e5b867d6980dc8
cxinping <templarz334422>
2019-09-29 14:50:15 +0800

    布隆过滤器

--- Chapter11/RedisbloomDemo/src/main/java/com/dxtd/redis/RedisbloomDemo.java
+++ Chapter11/RedisbloomDemo/src/main/java/com/dxtd/redis/RedisbloomDemo.java
@@ -6,25 +6,31 @@ public class RedisbloomDemo {
     public static void main(String[] args) {
         String userIdBloomKey = "userid";
         // 创建客户端，jedis实例
-        Client client = new Client("192.168.11.12", 6378);
+        Client client = new Client("192.168.11.12", 6379);
 
         // 创建一个有初始值和出错率的过滤器
         client.createFilter(userIdBloomKey,100000,0.01);
         // 新增一个<key,value>
-        boolean userid1 = client.add(userIdBloomKey,"101310222");
+        boolean userid1 = client.add(userIdBloomKey,"101310111");
         System.out.println("userid1 add " + userid1);
 
         // 批量新增values
-        boolean[] booleans = client.addMulti(userIdBloomKey, "101310111", "101310222", "101310222");
-        System.out.println("add multi result " + booleans);
+        boolean[] booleans = client.addMulti(userIdBloomKey, "101310222", "101310333", "101310444");
+
+        for (boolean flag : booleans){
+            System.out.println("add multi result " + flag );
+        }
 
         // 某个value是否存在
         boolean exists = client.exists(userIdBloomKey, "101310111");
-        System.out.println("101310111 是否存在" + exists);
+        System.out.println("101310111 是否存在 " + exists);
 
         //某批value是否存在
-        boolean existsBoolean[] = client.existsMulti(userIdBloomKey, "101310111","101310222", "101310222","11111111");
-        System.out.println("某批value是否存在 " + existsBoolean);
+        boolean existsBoolean[] = client.existsMulti(userIdBloomKey, "101310111","101310222", "101310333","101310555");
+
+        for (boolean flag : existsBoolean){
+            System.out.println("某批value是否存在 " + flag );
+        }
     }
 
 }
