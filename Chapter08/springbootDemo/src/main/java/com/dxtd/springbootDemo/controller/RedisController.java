a6ee7c1d1234fd1a3e35ae9cbb4cdcaee5460d55
cxinping <templarz334422>
2019-09-30 19:03:45 +0800

    SpringBoot 集成Redis

--- Chapter08/springbootDemo/src/main/java/com/dxtd/springbootDemo/controller/RedisController.java
+++ Chapter08/springbootDemo/src/main/java/com/dxtd/springbootDemo/controller/RedisController.java
@@ -1,5 +1,6 @@
 package com.dxtd.springbootDemo.controller;
 
+import com.dxtd.springbootDemo.RedisService;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,6 +15,9 @@ public class RedisController {
     @Resource
     private RedisTemplate redisTemplate;
 
+    @Resource
+    private RedisService service;
+
     @RequestMapping("/redis/setAndGet")
     @ResponseBody
     public Map setAndGetValue(String name, String value){
@@ -21,6 +25,16 @@ public class RedisController {
         redisTemplate.opsForValue().set(name,value);
         Map result = new HashMap();
         String getValue =  (String) redisTemplate.opsForValue().get(name);
+        result.put(name ,getValue);
+        return result;
+    }
+
+    @RequestMapping("/redis/setAndGet2")
+    @ResponseBody
+    public Map setAndGetValueV2(String name,String value){
+        service.set(name,value);
+        Map result = new HashMap();
+        String getValue =  service.get(name).toString();;
         result.put(name ,getValue);
         return result;
     }
