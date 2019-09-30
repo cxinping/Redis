d3b9370409ea339eb3ec6bac6e3795744c9deca2
cxinping <templarz334422>
2019-09-30 13:45:42 +0800

    SpringBoot 集成Redis

---
+++ Chapter08/springbootDemo/src/main/java/com/dxtd/springbootDemo/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.dxtd.springbootDemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String,String> redisTemplate(RedisConnectionFactory factory){
+        RedisTemplate<String,String> redisTemplate = new RedisTemplate<String,String>();
+        redisTemplate.setConnectionFactory(factory);
+        return redisTemplate;
+    }
+
+
+
+
+
+
+
+
+}
