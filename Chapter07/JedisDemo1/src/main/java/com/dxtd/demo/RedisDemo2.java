1e55df69026139154ff58625ea930bd8827375f4
cxinping <templarz334422>
2019-10-01 13:16:11 +0800

    jedis操作Redis

--- Chapter07/JedisDemo1/src/main/java/com/dxtd/demo/RedisDemo2.java
+++ Chapter07/JedisDemo1/src/main/java/com/dxtd/demo/RedisDemo2.java
@@ -2,6 +2,11 @@ package com.dxtd.demo;
 
 import redis.clients.jedis.Jedis;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 public class RedisDemo2 {
     public static void testString() {
         // 连接Redis服务器，127.0.0.1:6379
@@ -22,8 +27,39 @@ public class RedisDemo2 {
 
     }
 
+    public static void testMap() {
+        // -----添加数据----------
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("name", "wang");
+        map.put("age", "22");
+        map.put("qq", "123456");
+
+        // 连接redis服务器，127.0.0.1:6379
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        jedis.hmset("user", map);
+        // 取出user中的name，执行结果:[minxr]-->注意结果是一个泛型的List
+        // 第一个参数是存入redis中map对象的key，后面跟的是放入map中的对象的key，后面的key可以跟多个，是可变参数
+        List<String> rsmap = jedis.hmget("user", "name", "age", "qq");
+
+        // 删除map中的某个键值
+        jedis.hdel("user", "age");
+        System.out.println(jedis.hmget("user", "age")); // 因为删除了，所以返回的是null
+        System.out.println(jedis.hlen("user")); // 返回key为user的键中存放的值的个数2
+        System.out.println(jedis.exists("user"));// 是否存在key为user的记录 返回true
+        System.out.println(jedis.hkeys("user"));// 返回map对象中的所有key
+        System.out.println(jedis.hvals("user"));// 返回map对象中的所有value
+
+        Iterator<String> iter = jedis.hkeys("user").iterator();
+        while (iter.hasNext()) {
+            String key = iter.next();
+            System.out.println(key + ":" + jedis.hmget("user", key));
+        }
+    }
+
+
     public static void  main(String[] args){
-        testString();
+        //testString();
+        testMap();
 
     }
 
