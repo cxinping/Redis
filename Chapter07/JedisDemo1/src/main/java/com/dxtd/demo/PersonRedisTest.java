5b46fd6be56531a8d5e75a0262b58a7bdbc03a34
cxinping <templarz334422>
2019-10-01 15:53:59 +0800

    jedis操作Redis

---
+++ Chapter07/JedisDemo1/src/main/java/com/dxtd/demo/PersonRedisTest.java
@@ -0,0 +1,51 @@
+package com.dxtd.demo;
+
+import redis.clients.jedis.Jedis;
+
+public class PersonRedisTest {
+
+    private static Jedis jedis = null;
+
+    /**
+     * 初始化Jedis对象
+     *
+     * @throws Exception
+     */
+    public PersonRedisTest() {
+        jedis = new Jedis("127.0.0.1", 6379);
+    }
+
+    /**
+     * 序列化写对象, 将Person对象写入Redis中
+     *
+     */
+    public void setObject() {
+        jedis.set("person:100".getBytes(),
+                SerializeUtil.serialize(new Person(100, "zhangsan")));
+        jedis.set("person:101".getBytes(),
+                SerializeUtil.serialize(new Person(101, "xinping")));
+    }
+
+    /**
+     * 反序列化取对象, 用Jedis获取对象
+     *
+     */
+    public void getObject() {
+        byte[] data100 = jedis.get(("person:100").getBytes());
+        Person person100 = (Person) SerializeUtil.unserialize(data100);
+        System.out.println(String.format("person:100->id=%s,name=%s",
+                person100.getId(), person100.getName()));
+
+        byte[] data101 = jedis.get(("person:101").getBytes());
+        Person person101 = (Person) SerializeUtil.unserialize(data101);
+        System.out.println(String.format("person:101->id=%s,name=%s",
+                person101.getId(), person101.getName()));
+    }
+
+    public static void main(String[] args) {
+        PersonRedisTest rt = new PersonRedisTest();
+        rt.setObject();
+        rt.getObject();
+    }
+
+}
---
+++ Chapter07/JedisDemo1/src/main/java/com/dxtd/demo/RedisTransDemo.java
@@ -0,0 +1,57 @@
+package com.dxtd.demo;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
+import redis.clients.jedis.Transaction;
+
+import java.util.List;
+
+public class RedisTransDemo
+{
+    public static void jedisNormal() {
+        Jedis jedis = new Jedis("localhost");
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 10; i++) {
+            String result = jedis.set("n:" + i,   ""+i);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Simple SET: " + ((end - start)/1000.0) + " seconds");
+        jedis.disconnect();
+    }
+
+
+    public static void jedisTrans() {
+        Jedis jedis = new Jedis("localhost");
+        long start = System.currentTimeMillis();
+        Transaction tx = jedis.multi();
+        for (int i = 0; i < 100; i++) {
+            tx.set("t:" + i,  i + "");
+        }
+        List<Object> results = tx.exec();
+        long end = System.currentTimeMillis();
+        System.out.println("Transaction SET: " + ((end - start)/1000.0) + " seconds");
+        jedis.disconnect();
+    }
+
+    public static  void jedisPipelined() {
+        Jedis jedis = new Jedis("localhost");
+        Pipeline pipeline = jedis.pipelined();
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            pipeline.set("p:" + i, "" + i);
+        }
+        List<Object> results = pipeline.syncAndReturnAll();
+        long end = System.currentTimeMillis();
+        System.out.println("Pipelined SET: " + ((end - start)/1000.0) + " seconds");
+        jedis.disconnect();
+    }
+
+
+    public static void main(String[] args){
+        //jedisNormal();
+        //jedisTrans();
+        jedisPipelined();
+
+    }
+
+}
