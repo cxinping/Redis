fd7b17d3cd43d475779ad22123a5ffd7a7045c7c
cxinping <templarz334422>
2019-10-02 21:42:04 +0800

    激动Redis 集群

---
+++ Chapter06/startRedisCluster.sh
@@ -0,0 +1,7 @@
+redis-server /usr/local/redis-cluster/8001/redis.conf 
+redis-server /usr/local/redis-cluster/8002/redis.conf 
+redis-server /usr/local/redis-cluster/8003/redis.conf 
+redis-server /usr/local/redis-cluster/8004/redis.conf 
+redis-server /usr/local/redis-cluster/8005/redis.conf 
+redis-server /usr/local/redis-cluster/8006/redis.conf
+
