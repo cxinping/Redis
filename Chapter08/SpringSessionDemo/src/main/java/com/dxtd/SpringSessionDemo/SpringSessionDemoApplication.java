6f3fdb33e5e9dad93f681e43a67a9f8f32b1c40a
cxinping <templarz334422>
2019-10-08 12:42:44 +0800

    SpringBoot集成Spring Session

---
+++ Chapter08/SpringSessionDemo/src/main/java/com/dxtd/SpringSessionDemo/SpringSessionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dxtd.SpringSessionDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSessionDemoApplication.class, args);
+	}
+
+}
