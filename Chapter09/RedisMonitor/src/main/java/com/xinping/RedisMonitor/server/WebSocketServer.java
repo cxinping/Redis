44e92ceef83439198d7b17bc1b132b2c97165f65
cxinping <xpws2006@163.com>
2020-08-09 16:58:39 +0800

    优化监控

--- Chapter09/RedisMonitor/src/main/java/com/xinping/RedisMonitor/server/WebSocketServer.java
+++ Chapter09/RedisMonitor/src/main/java/com/xinping/RedisMonitor/server/WebSocketServer.java
@@ -22,7 +22,7 @@ public class WebSocketServer {
     private static AtomicInteger onlineCount = new AtomicInteger(0);
 
     /**
-     * concurrent包的线程安全Set，用来存放每个客户端对应的 WebSocketServer  对象。若要实现服务端与单一客户端通信的话，可以使用Map来存放，其中Key可以为用户标识
+     * 使用CopyOnWriteArraySet对象，concurrent包的线程安全Set，用来存放每个客户端对应的 WebSocketServer  对象。若要实现服务端与单一客户端通信的话，可以使用Map来存放，其中Key可以为用户标识
      */
     private static CopyOnWriteArraySet<WebSocketServer> webSocketSet = new CopyOnWriteArraySet<WebSocketServer>();
 
